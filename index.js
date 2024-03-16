1370526ab23545f9460edb1b3e9d45aa2d27635c
omsant02 <omsantoshwar02@gmail.com>
2024-03-16 21:35:48 +0530

    ded

--- index.js
+++ index.js
@@ -2,20 +2,15 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-const staticFolderPath = path.join(__dirname, "static");
-
-// Serve static files from the 'static' folder
-app.use(express.static(staticFolderPath));
+const txtFilePath = path.join(__dirname, "identity-connect.txt");
 
 app.get("/identity-connect.txt", (req, res) => {
-  const txtFilePath = path.join(staticFolderPath, "identity-connect.txt");
   res.sendFile(txtFilePath);
 });
-
 app.get("/", (req, res) => {
   res.send("hello");
 });
 
 app.listen(8000, () => {
-  console.log("Server is running on port 8000");
+  console.log("Server is running on port 3000");
 });
