1c14eb09ac78f0f11330bd6fb6735916841040e2
omsant02 <omsantoshwar02@gmail.com>
2024-03-16 21:17:20 +0530

    ic

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+const txtFilePath = path.join(__dirname, "identity-connect.txt");
+
+app.get("/identity-connect.txt", (req, res) => {
+  res.sendFile(txtFilePath);
+});
+app.get("/", (req, res) => {
+  res.send("hello");
+});
+
+app.listen(8000, () => {
+  console.log("Server is running on port 8000");
+});
